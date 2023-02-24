b825480844a860e4b2c4d3ead19a58dee35950a3
AleksPastukhov <passandro1990@gmail.com>
2023-02-24 23:35:48 +0200

    add formik

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,56 +1,50 @@
-import { useState } from 'react';
+import { Formik, Field, ErrorMessage } from 'formik';
 import PropTypes from 'prop-types';
-import { Label, Button, Form, Input, Span } from './ContactForm.styled';
+import { UserForm } from './ContactForm.styled';
+import { object, string } from 'yup';
+
+const userSchema = object({
+  name: string()
+    .matches(
+      /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
+      "Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+    )
+    .required(),
+  number: string()
+    .matches(
+      /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/,
+      'Phone number must be digits and can contain spaces, dashes, parentheses and can start with +'
+    )
+    .required(),
+});
 
 export default function ContactForm({ onSubmit }) {
-  const [name, setName] = useState('');
-  const [number, setNumber] = useState('');
-
-  const handleChange = e => {
-    const inputName = e.currentTarget.name;
-    const { value } = e.currentTarget;
-    if (inputName === 'name') {
-      setName(value);
-    } else {
-      setNumber(value);
-    }
-  };
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    onSubmit(name, number);
-    setName('');
-    setNumber('');
+  const handleSubmit = (e, { resetForm }) => {
+    console.log(e);
+    onSubmit(e.name, e.number);
+    resetForm();
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <Label>
-        <Span>Name</Span>
-        <Input
-          type="text"
-          name="name"
-          value={name}
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-          onChange={handleChange}
-        />
-      </Label>
-      <Label>
-        <Span>Number</Span>
-        <Input
-          type="tel"
-          name="number"
-          value={number}
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          required
-          onChange={handleChange}
-        />
-      </Label>
-      <Button type="submit">Add contact</Button>
-    </Form>
+    <Formik
+      initialValues={{ name: '', number: '' }}
+      onSubmit={handleSubmit}
+      validationSchema={userSchema}
+    >
+      <UserForm>
+        <label>
+          <span>Name</span>
+          <Field type="text" name="name" required />
+          <ErrorMessage name="name" />
+        </label>
+        <label>
+          <span>Number</span>
+          <Field type="tel" name="number" required />
+          <ErrorMessage name="number" />
+        </label>
+        <button type="submit">Add contact</button>
+      </UserForm>
+    </Formik>
   );
 }
 
--- src/components/ContactForm/ContactForm.styled.jsx
+++ src/components/ContactForm/ContactForm.styled.jsx
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled';
+import { Form } from 'formik';
 
-export const Form = styled.form`
+export const UserForm = styled(Form)`
   background-color: ${p => p.theme.colors.backgroundColors};
   color: ${p => p.theme.colors.colorText};
   padding: 20px;
@@ -10,43 +11,44 @@ export const Form = styled.form`
   // margin: 0 auto;
   box-shadow: ${p => p.theme.sectionShadow};
   border-radius: 10px;
-`;
 
-export const Label = styled.label`
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 10px;
-`;
-export const Span = styled.span`
-  margin-bottom: 3px;
-`;
-
-export const Input = styled.input`
-  background-color: ${p => p.theme.colors.colorElement};
-  color: ${p => p.theme.colors.btnСolorText};
-  padding: 5px;
-  font-weight: 700;
-  &:hover,
-  &:active,
-  &:focus {
-    background-color: ${p => p.theme.colors.white};
-    outline: transparent;
+  & label {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 10px;
   }
-`;
 
-export const Button = styled.button`
-  width: 150px;
-  margin-left: auto;
-  margin-right: auto;
-  color: ${p => p.theme.colors.btnСolorText};
-  background-color: ${p => p.theme.colors.colorElement};
-  border-radius: 20px;
-  padding: 7px 10px;
-  border: 0;
-  font-size: 18px;
-  transition: box-shadow 250ms ease, transform 250ms ease;
-  &:hover {
-    transform: scale(1.1);
-    box-shadow: ${p => p.theme.boxShadow};
+  & span {
+    margin-bottom: 3px;
+  }
+
+  & input {
+    background-color: ${p => p.theme.colors.colorElement};
+    color: ${p => p.theme.colors.btnСolorText};
+    padding: 5px;
+    font-weight: 700;
+    &:hover,
+    &:active,
+    &:focus {
+      background-color: ${p => p.theme.colors.white};
+      outline: transparent;
+    }
+  }
+
+  & button {
+    width: 150px;
+    margin-left: auto;
+    margin-right: auto;
+    color: ${p => p.theme.colors.btnСolorText};
+    background-color: ${p => p.theme.colors.colorElement};
+    border-radius: 20px;
+    padding: 7px 10px;
+    border: 0;
+    font-size: 18px;
+    transition: box-shadow 250ms ease, transform 250ms ease;
+    &:hover {
+      transform: scale(1.1);
+      box-shadow: ${p => p.theme.boxShadow};
+    }
   }
 `;
